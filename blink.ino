af275b89592b4bec626dcd68eae4cc4e394f3972
mudongzishiba <abcde1024359354@gmail.com>
2024-12-18 18:31:32 +0800

    1.234 seconds is better

--- blink.ino
+++ blink.ino
@@ -18,7 +18,7 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(300);               // wait for a second
+  delay(1234);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(300);               // wait for a second
+  delay(1234);               // wait for a second
 }
